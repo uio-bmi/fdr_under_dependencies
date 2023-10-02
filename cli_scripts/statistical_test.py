6001a532bd24262d6044f35a807a8627270f5951
kanduric <chakri.co@gmail.com>
2023-10-02 15:11:22 +0200

    finally working version of params space exploration-based data generation + statistical testing

--- cli_scripts/statistical_test.py
+++ cli_scripts/statistical_test.py
@@ -1,16 +1,17 @@
 import argparse
 from fdr_hacking.statistical_testing import *
-from fdr_hacking.util import parse_yaml_file
 import numpy as np
+import json
+import pandas as pd
 
 
 def execute():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', help='Configuration in YAML format', required=True)
+    parser.add_argument('--config', type=str, help='Configuration in YAML format', required=True)
     parser.add_argument('--dataset', help='Path to a tab-delimited dataset in the form of a design matrix', required=True)
     parser.add_argument('--output', help='Path to the output file', required=True)
     args = parser.parse_args()
-    config = parse_yaml_file(args.config)
+    config = json.loads(args.config.replace("\'", "\""))
     data = np.loadtxt(args.dataset, delimiter="\t")
     n_obs = data.shape[0]
     group_size = n_obs // 2
@@ -18,6 +19,8 @@ def execute():
                                             group1_indices = list(range(group_size)),
                                             group2_indices = list(range(group_size, n_obs)),
                                             test_type=config['statistical_test'],
-                                            method=config['multipletest_correction_type'],
+                                            method=config['multiple_test_correction'],
                                             alpha=config['alpha'])
-    np.savetxt(args.output, np.array([num_significant_findings], dtype=np.int64), fmt="%d", delimiter="\t")
+    config['num_significant_findings'] = num_significant_findings
+    pd.DataFrame(config).to_csv(args.output, sep="\t", index=True, header=True)
+    # np.savetxt(args.output, np.array([num_significant_findings], dtype=np.int64), fmt="%d", delimiter="\t")
