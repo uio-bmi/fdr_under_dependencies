54b6978374785381d33abd2cc612b7ee9392a115
mmamica <maria.anna.mamica@gmail.com>
2023-11-23 13:17:58 +0100

    fix: fix labels of conditional histograms and improve the charts readability

--- scripts/cli_scripts/plot_histograms.py
+++ scripts/cli_scripts/plot_histograms.py
@@ -23,19 +23,24 @@ def execute():
     if not os.path.exists(args.output_dir):
         os.mkdir(args.output_dir)
     for index, row in df.iterrows():
+        custom_tick_labels = []
         if args.remove_zero_bin is True:
             row['reporting_histogram'] = row['reporting_histogram'][1:]
             row['reporting_histogram_bins'] = row['reporting_histogram_bins'][1:]
+            start_bin = 0
+        else:
+            custom_tick_labels.append(bin_edges[0])
+            start_bin = 1
         num_bins = len(row['reporting_histogram'])
         bin_edges = row['reporting_histogram_bins']
-        custom_tick_labels = [bin_edges[0]]
-        for i in range(1, num_bins): # [0, 1, 10, 100, 1000]
+
+        for i in range(start_bin, num_bins): # [0, 1, 10, 100, 1000]
             if i == num_bins-1:
                 custom_tick_labels.append(f'>= {bin_edges[i]}')
             else:
                 custom_tick_labels.append(f'{bin_edges[i]}-{bin_edges[i + 1]-1}')
         custom_tick_labels = custom_tick_labels + [f'{bin_edges[i]:.1f}-{bin_edges[i + 1]:.1f}' for i in range(1, num_bins)]
-        hist_trace = go.Bar(x=custom_tick_labels, y=row['reporting_histogram'])
+        hist_trace = go.Bar(x=custom_tick_labels, y=row['reporting_histogram'], marker= dict(color='#4682B4'))
         relevant_cols = ["n_observations", "n_sites", "dependencies", "correlation_strength", "bin_size_ratio",
                        "statistical_test", "multipletest_correction_type", "alpha", "data_distribution"]
         config_cols = [i for i in relevant_cols if i in df.columns]
@@ -46,13 +51,13 @@ def execute():
                 title={'text': formatted_title, 'y': 0.95},
                 title_pad=dict(b=200),
                 margin=dict(t=250),
-                xaxis=dict(title='Number of false findings', tickvals=custom_tick_labels, ticktext=custom_tick_labels),
-                yaxis=dict(title='Number of datasets')
+                xaxis=dict(title='Number of false findings', tickvals=custom_tick_labels, ticktext=custom_tick_labels, titlefont=dict(size=26), tickfont=dict(size=22)),
+                yaxis=dict(title='Number of datasets', titlefont=dict(size=26), tickfont=dict(size=22))
             )
         else:
             layout = go.Layout(
-                xaxis=dict(title='Number of false findings', tickvals=custom_tick_labels, ticktext=custom_tick_labels),
-                yaxis=dict(title='Number of datasets')
+                xaxis=dict(title='Number of false findings', tickvals=custom_tick_labels, ticktext=custom_tick_labels, titlefont=dict(size=26), tickfont=dict(size=22)),
+                yaxis=dict(title='Number of datasets', titlefont=dict(size=26), tickfont=dict(size=22))
             )
 
         fig = go.Figure(data=[hist_trace], layout=layout)
