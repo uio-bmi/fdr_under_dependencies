6717808209a41e83188373f2454aee1fbbb4c7eb
mmamica <maria.anna.mamica@gmail.com>
2023-12-14 09:33:25 +0100

    test: rewrite utils tests to pytest

--- tests/analysis/test_utils.py
+++ tests/analysis/test_utils.py
@@ -1,66 +1,72 @@
 import os
 import tempfile
-import unittest
-from unittest.mock import patch
-
+import pytest
 import yaml
 
 from scripts.analysis.utils import parse_yaml_file
 
 
-class TestParseYAMLFile(unittest.TestCase):
+@pytest.fixture
+def valid_yaml_content():
+    return """
+    key1: value1
+    key2: value2
+    """
 
-    def setUp(self):
-        self.valid_yaml_content = """
-        key1: value1
-        key2: value2
-        """
-        self.invalid_yaml_content = ":"
 
-    def test_parse_valid_yaml_file(self):
+@pytest.fixture
+def invalid_yaml_content():
+    return ":"
+
+
+@pytest.fixture
+def create_temp_yaml_file():
+    def _create_temp_yaml_file(content):
         with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
-            temp_file.write(self.valid_yaml_content)
-            temp_file_path = temp_file.name
+            temp_file.write(content)
+            return temp_file.name
 
-        try:
-            result = parse_yaml_file(temp_file_path)
-            expected_result = {'key1': 'value1', 'key2': 'value2'}
-            self.assertEqual(result, expected_result)
-        finally:
-            os.remove(temp_file_path)
+    return _create_temp_yaml_file
 
-    def test_parse_empty_yaml_file(self):
-        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
-            temp_file_path = temp_file.name
 
-        with self.assertRaises(ValueError) as context:
-            parse_yaml_file(temp_file_path)
+def test_parse_valid_yaml_file(valid_yaml_content, create_temp_yaml_file):
+    temp_file_path = create_temp_yaml_file(valid_yaml_content)
 
-        self.assertIn("The supplied YAML file", str(context.exception))
-        os.remove(temp_file_path)
+    result = parse_yaml_file(temp_file_path)
+    expected_result = {'key1': 'value1', 'key2': 'value2'}
+    assert result == expected_result
 
-    def test_parse_invalid_yaml_file(self):
-        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
-            temp_file.write(self.invalid_yaml_content)
-            temp_file_path = temp_file.name
+    os.remove(temp_file_path)
 
-        with self.assertRaises(ValueError) as context:
-            parse_yaml_file(temp_file_path)
 
-        self.assertIn("Error parsing YAML", str(context.exception))
-        os.remove(temp_file_path)
+def test_parse_empty_yaml_file(create_temp_yaml_file):
+    temp_file_path = create_temp_yaml_file("")
 
-    @patch('yaml.safe_load', side_effect=yaml.YAMLError())
-    def test_yaml_parsing_exception(self, mock_safe_load):
-        with tempfile.NamedTemporaryFile(mode='w+', delete=False) as temp_file:
-            temp_file.write(self.valid_yaml_content)
-            temp_file_path = temp_file.name
+    with pytest.raises(ValueError, match="The supplied YAML file"):
+        parse_yaml_file(temp_file_path)
 
-        with self.assertRaises(ValueError) as context:
-            parse_yaml_file(temp_file_path)
+    os.remove(temp_file_path)
 
-        self.assertIn("Error parsing YAML", str(context.exception))
-        os.remove(temp_file_path)
 
-if __name__ == '__main__':
-    unittest.main()
+def test_parse_invalid_yaml_file(invalid_yaml_content, create_temp_yaml_file):
+    temp_file_path = create_temp_yaml_file(invalid_yaml_content)
+
+    with pytest.raises(ValueError, match="Error parsing YAML"):
+        parse_yaml_file(temp_file_path)
+
+    os.remove(temp_file_path)
+
+
+def test_yaml_parsing_exception(valid_yaml_content, create_temp_yaml_file, monkeypatch):
+    temp_file_path = create_temp_yaml_file(valid_yaml_content)
+
+    def mock_safe_load_raise_error(x):
+        raise yaml.YAMLError()
+
+    monkeypatch.setattr(yaml, 'safe_load', mock_safe_load_raise_error)
+
+    with pytest.raises(ValueError, match="Error parsing YAML"):
+        parse_yaml_file(temp_file_path)
+
+    os.remove(temp_file_path)
+
