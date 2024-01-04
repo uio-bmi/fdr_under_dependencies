1f6a3f66e39cb71401142c10460228f12b192565
mmamica <maria.anna.mamica@gmail.com>
2024-01-04 10:17:15 +0100

    test: ensuring 100% code coverage for statistical_analysis script

--- tests/analysis/test_statistical_analysis.py
+++ tests/analysis/test_statistical_analysis.py
@@ -1,9 +1,50 @@
 import numpy as np
 import pytest
 from scripts.analysis.statistical_analysis import perform_t_test, perform_limma_test, perform_rank_sum_test, \
-    perform_ks_test, get_p_values, adjust_p_values, count_significant_p_values
+    perform_ks_test, get_p_values, adjust_p_values, count_significant_p_values, quantify_significance
 
 MODULE_PATH = 'scripts.analysis.statistical_analysis'
+
+
+@pytest.fixture
+def mock_sample_data():
+    return np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [10.0, 11.0, 12.0]])
+
+
+@pytest.fixture
+def mock_group1_indices():
+    return [0, 1]
+
+
+@pytest.fixture
+def mock_group2_indices():
+    return [2, 3]
+
+
+@pytest.fixture
+def mock_p_values():
+    return np.array([0.7, 0.7, 0.7])
+
+
+@pytest.fixture
+def mock_adjusted_p_values():
+    return np.array([0.7, 0.5, 0.2])
+
+
+@pytest.fixture
+def mock_alpha():
+    return 0.5
+
+
+@pytest.fixture
+def mock_test_type():
+    return 't-test'
+
+
+@pytest.fixture
+def mock_correction_method():
+    return 'bonferroni'
+
 
 @pytest.fixture
 def mock_statistical_tests(mocker):
@@ -33,7 +74,8 @@ def mock_kstest(mocker):
 
 @pytest.fixture
 def mock_multipletests(mocker):
-    return mocker.patch(f'{MODULE_PATH}.multipletests', return_value=(mocker.MagicMock,np.array([0.5, 0.5, 0.5]), mocker.MagicMock, mocker.MagicMock))
+    return mocker.patch(f'{MODULE_PATH}.multipletests',
+                        return_value=(mocker.MagicMock, np.array([0.5, 0.5, 0.5]), mocker.MagicMock, mocker.MagicMock))
 
 
 @pytest.fixture
@@ -46,28 +88,18 @@ def mock_r_functions(mocker):
 
 
 @pytest.fixture
-def mock_sample_data():
-    return np.array([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0], [7.0, 8.0, 9.0], [10.0, 11.0, 12.0]])
+def mock_get_p_values(mocker):
+    return mocker.patch(f'{MODULE_PATH}.get_p_values', return_value=np.array([0.7, 0.7, 0.7]))
 
 
 @pytest.fixture
-def mock_group1_indices():
-    return [0, 1]
+def mock_adjust_p_values(mocker):
+    return mocker.patch(f'{MODULE_PATH}.adjust_p_values', return_value=np.array([0.4, 0.4, 0.4]))
 
 
 @pytest.fixture
-def mock_group2_indices():
-    return [2, 3]
-
-
-@pytest.fixture
-def mock_p_values():
-    return np.array([0.7, 0.7, 0.7])
-
-
-@pytest.fixture
-def mock_adjusted_p_values():
-    return np.array([0.7, 0.5, 0.2])
+def mock_count_significant_p_values(mocker):
+    return mocker.patch(f'{MODULE_PATH}.count_significant_p_values', return_value=3)
 
 
 def test_perform_t_test(mock_sample_data, mock_group1_indices, mock_group2_indices):
@@ -152,8 +184,9 @@ def test_get_p_values_valid(test_type, mock_sample_data, mock_group1_indices, mo
     ('rank-sum', np.array([0.3, 0.3, 0.3])),
     ('ks-test', np.array([0.2, 0.2, 0.2])),
 ])
-def test_get_p_values_valid_with_mock(test_type, expected_results, mock_statistical_tests, mock_sample_data, mock_group1_indices,
-                                       mock_group2_indices):
+def test_get_p_values_valid_with_mock(test_type, expected_results, mock_statistical_tests, mock_sample_data,
+                                      mock_group1_indices,
+                                      mock_group2_indices):
     p_values = get_p_values(mock_sample_data, mock_group1_indices, mock_group2_indices, test_type)
     assert mock_statistical_tests[test_type].call_count == 1
     assert isinstance(p_values, np.ndarray)
@@ -200,6 +233,26 @@ def test_adjust_p_values_invalid_method(mock_p_values):
 def test_count_significant_p_values(mock_adjusted_p_values, alpha, expected_count):
     actual_count = count_significant_p_values(mock_adjusted_p_values, alpha)
     assert actual_count == expected_count
+
+
+def test_quantify_significance(mock_sample_data, mock_group1_indices, mock_group2_indices, mock_test_type,
+                                         mock_correction_method, mock_alpha):
+    significant_findings_number = quantify_significance(mock_sample_data, mock_group1_indices, mock_group2_indices,
+                                                        mock_test_type,
+                                                        mock_correction_method, mock_alpha)
+    assert significant_findings_number <= mock_sample_data.shape[1]
+
+
+def test_quantify_significance_with_mock(mock_sample_data, mock_group1_indices, mock_group2_indices, mock_test_type,
+                                         mock_correction_method, mock_alpha, mock_get_p_values, mock_adjust_p_values,
+                                         mock_count_significant_p_values):
+    significant_findings_number = quantify_significance(mock_sample_data, mock_group1_indices, mock_group2_indices,
+                                                        mock_test_type,
+                                                        mock_correction_method, mock_alpha)
+    assert mock_get_p_values.call_count == 1
+    assert mock_adjust_p_values.call_count == 1
+    assert mock_count_significant_p_values.call_count == 1
+    assert significant_findings_number == 3
 
 
 if __name__ == '__main__':
